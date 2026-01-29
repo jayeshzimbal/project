44e970d639a1200e67fd45ff1298f08cdd92a670
Jayesh <jayeshzimbal@gmail.com>
2026-01-29 10:42:51 +0530

    deploy py app using git-action cicd

---
+++ git-action/app.py
@@ -0,0 +1,14 @@
+import datetime
+
+def main():
+    print("===================================")
+    print("  Python Application Deployment")
+    print("===================================")
+    print("Hello from Python")
+    print("Status : Application is running")
+    print("Deployed using : GitHub Actions")
+    print("Time :", datetime.datetime.now())
+    print("===================================")
+
+if __name__ == "__main__":
+    main()
